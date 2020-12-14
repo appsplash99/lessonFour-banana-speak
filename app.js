164d0e891d7fce429f6eee5c6c1b0786ae0a62a1
appsplash99 <apurvchimralwar1997@gmail.com>
2020-12-14 20:58:51 +0530

    created app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+// console.log("Check if Script is working from different file")
+
+// alert("This Script Works");
+
+// ex:2 Using Javascript
+var userName = prompt("Gimme ur UserName:");    // input
+var greet = "Welcome " + userName;          // processing
+alert(greet);                               // output
+
+// ex:3 wiring button click
+
