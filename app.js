8899d20e5be4344bd79f249b9d8c6c6d84b61d77
appsplash99 <apurvchimralwar1997@gmail.com>
2020-12-14 21:50:17 +0530

    ex3

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
+// ex:1
 // console.log("Check if Script is working from different file")
-
 // alert("This Script Works");
 
-// ex:2 Using Javascript
-var userName = prompt("Gimme ur UserName:");    // input
-var greet = "Welcome " + userName;          // processing
-alert(greet);                               // output
 
-// ex:3 wiring button click
+// ex:2 Using Javascript
+// var userName = prompt("Gimme ur UserName:");    // input
+// var greet = "Welcome " + userName;          // processing
+// alert(greet);                               // output
+
+
+/*ex:3 wiring button click*/
+
+// reference to the "button id" in js
+var btnTranslate = document
+                    .querySelector("#btn-translate");
+
+// listening to an event and
+// telling browser whenever there's a click event perform this function
+btnTranslate.addEventListener(
+  "click",
+  function clickEventHandler() {
+    console.log("Clicked")
+  }
+)
 
